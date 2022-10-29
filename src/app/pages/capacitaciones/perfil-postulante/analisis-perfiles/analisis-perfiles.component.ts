a24dbceacd5d70aec7a0050610fda5f52be2f70e
ecastillosilupu <castillosilupu@hotmail.com>
2022-10-29 09:48:40 -0500

    CAMBIAR LISTA DE CURSOS - ANALISIS DE ADMINISTRADOS

--- src/app/pages/capacitaciones/perfil-postulante/analisis-perfiles/analisis-perfiles.component.ts
+++ src/app/pages/capacitaciones/perfil-postulante/analisis-perfiles/analisis-perfiles.component.ts
@@ -21,6 +21,58 @@ export class AnalisisPerfilesComponent implements OnInit {
   public boolOp = true
   public arrAdministrados : administrado[] = []
 
+  arrCursos: any [] = [
+    {        
+      nivel: 'Básico',
+      nombre : 'Curso Básico del Sistema de Comando de Incidentes',
+      descripcion : 'Este curso es para reforzar el liderazgos frente a peligros de incendios forestales',
+      codigo : 'D-311',
+    },
+    {        
+      nivel: 'Básico',
+      nombre : 'Primera Respuesta a Incidentes con Materiales Peligrosos',
+      descripcion : 'Este curso es para reforzar el liderazgos frente a peligros de incendios forestales',
+      codigo : 'P-101',
+    },
+    {        
+      nivel: 'Intermedio',
+      nombre : 'Rescate con Cuerdas',
+      descripcion : 'Este curso es para reforzar el liderazgos frente a peligros de incendios forestales',
+      codigo : 'FI-110',
+    },
+    {        
+      nivel: 'Intermedio',
+      nombre : 'Curso de Primeros Auxilios y Atención PreHospitalaria',
+      descripcion : 'Este curso es para reforzar el liderazgos frente a peligros de incendios forestales',
+      codigo : 'FI-210',
+    },
+    {        
+      nivel: 'Básico',
+      nombre : 'Curso de rescate es estructuras colapsadas en el Nivel Liviano  ',
+      descripcion : 'Este curso es para reforzar el liderazgos frente a peligros de incendios forestales',
+      codigo : 'L-180',
+    },
+    {        
+      nivel: 'Básico',
+      nombre : 'Curso de rescate en espacios confinados',
+      descripcion : 'Este curso es para reforzar el liderazgos frente a peligros de incendios forestales',
+      codigo : 'L-280',
+    },
+    {        
+      nivel: 'Básico',
+      nombre : 'Curso de Instructores',
+      descripcion : 'Este curso es para reforzar el liderazgos frente a peligros de incendios forestales',
+      codigo : 'M-410',
+    },
+    {        
+      nivel: 'Básico',
+      nombre : 'Curso de búsqueda canina',
+      descripcion : 'Este curso es para reforzar el liderazgos frente a peligros de incendios forestales',
+      codigo : 'M-410',
+    },
+    
+  ]
+  
   constructor(
     private modalService : NgbModal,
   ) { }
