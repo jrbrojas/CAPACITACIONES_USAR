6d2d73d5ddd7fd7dacce394b8d8a1d16add81e80
ecastillosilupu <58020909+ecastillosilupu@users.noreply.github.com>
2022-11-03 23:32:17 -0600

    CAMBIO 13

--- src/app/pages/capacitaciones/perfil-postulante/add-perfiles/add-perfiles.component.ts
+++ src/app/pages/capacitaciones/perfil-postulante/add-perfiles/add-perfiles.component.ts
@@ -57,6 +57,119 @@ export class AddPerfilesComponent implements OnInit {
     }
   ];
 
+  arrCurso: any [] = [{
+    id: 1,
+    asignacion : 'Lider de Equipo',
+  },
+  {
+    id: 1,
+    asignacion : 'Lider de Equipo (Designado)',
+  },
+  {
+    id: 1,
+    asignacion : 'Oficial de Planificacion',
+  },
+  {
+    id: 1,
+    asignacion : 'Oficial de Seguridad',
+  },
+  {
+    id: 1,
+    asignacion : 'Lider de Tripulación',
+  },
+  {
+    id: 1,
+    asignacion : 'Equipio de Busqueda y Rescato (incluyendo perros)',
+  },
+  {
+    id: 1,
+    asignacion : 'Especializta Médico y/o Médico/Paramédico/Enfermera',
+  },
+  {
+    id: 1,
+    asignacion : 'Gestor del Equipo de Logística',
+  },
+  {
+    id: 1,
+    asignacion : 'Responsable de Logística',
+  },
+  {
+    id: 1,
+    asignacion : 'Lider de Equipo',
+  },
+  {
+    id: 1,
+    asignacion : 'Lider Adjunto',
+  },
+  {
+    id: 1,
+    asignacion : 'Oficial de Planificación',
+  },
+  {
+    id: 1,
+    asignacion : 'Oficial de Enlace',
+  },
+  {
+    id: 1,
+    asignacion : 'Ingeniero Estructural',
+  },
+  {
+    id: 1,
+    asignacion : 'Oficial de Seguridad',
+  },
+  {
+    id: 1,
+    asignacion : 'Oficial de Coordinación',
+  },
+  {
+    id: 1,
+    asignacion : 'Especializta de Búsqueda Técnica',
+  },
+  {
+    id: 1,
+    asignacion : 'Adiestrador de Perros',
+  },
+  {
+    id: 1,
+    asignacion : 'Especialista en MATPEL',
+  },
+  {
+    id: 1,
+    asignacion : 'Lider de Equipo de Rescate',
+  },
+  {
+    id: 1,
+    asignacion : 'Técnico de Rescate',
+  },
+  {
+    id: 1,
+    asignacion : 'Especialista en Aparejos Pesados',
+  },
+  {
+    id: 1,
+    asignacion : 'Especialista Médico y/o Médico/Paramédico/Enfermera',
+  },
+  {
+    id: 1,
+    asignacion : 'Lider de Equipo de Logística',
+  },
+  {
+    id: 1,
+    asignacion : 'Especialista en Transporte',
+  },
+  {
+    id: 1,
+    asignacion : 'Especialista en Logística',
+  },
+  {
+    id: 1,
+    asignacion : 'Administrador de Base',
+  },
+  {
+    id: 1,
+    asignacion : 'Especialista en Comunicación',
+  }]
+
   public data1: any = [
     {
       id: 1,
